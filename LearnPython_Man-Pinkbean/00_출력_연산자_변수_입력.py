22b9580b32808597f29fe3009bdf86f47943a120
pinkbean <68334603+pinkbean630@users.noreply.github.com>
2020-07-19 12:02:58 +0900

    Add files via upload

---
+++ LearnPython_Man-Pinkbean/00_출력_연산자_변수_입력.py
@@ -0,0 +1,53 @@
+# 출력
+print("Hello World!")
+
+print("저의 이름은 핑크빈 입니다.")
+
+# 연산자
+print(3+5)
+print(5-2)
+print(5*2)
+print(6/2)
+print(6%4)
+print(2**3)
+
+print("5 + 5은",5+5,"입니다.")
+print("뺄셈 : 30 - 20은", 30-20,"입니다.")
+print("곱셈 : 5 * 0은", 5 * 0,"입니다.")
+print("거듭제곱 : 2의 거듭제곱은", 2 ** 2,"입니다.")
+
+
+# 변수
+# 태어난 년ㄷ 출력
+age = 13
+year = 2020
+print(year-14)
+
+a = input("값을 넣어주세요 : ")
+print(a)
+
+
+# 덧셈 계산기
+N = input("N ? ")
+M = input("M ? ")
+Temp = N+M # 문자열로 더해짐
+print(Temp)
+
+# 형변환
+N = int(input("N ? "))
+M = int(input("M ? "))
+Temp = N+M
+print(N," + ",M," = ",Temp)
+
+A = 10.4
+print(type(sum))
+
+# 관계 연산자
+# >= <= < >
+print(3>5)
+print(3>=3)
+print(5>3)
+
+
+
+
